aac64f7b64985e472d70383247d807130501e67e
eraldoplimajr <eraldoplimajr@gmail.com>
2022-06-25 17:32:16 -0300

    Exercício criando repository e lombok para restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome) values ('Restaurante tailandês');
+insert into restaurante (nome) values ('Restaurante indiano');
