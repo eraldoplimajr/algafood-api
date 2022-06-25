fde789781d30aee068d198c23198f94667d4215a
eraldoplimajr <eraldoplimajr@gmail.com>
2022-06-25 17:54:25 -0300

    Mapeando relacionamento com ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
 
-insert into restaurante (nome) values ('Restaurante tailandês');
-insert into restaurante (nome) values ('Restaurante indiano');
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
