879763917fcc598b7c0dfbf404278418cd688367
eraldoplimajr <eraldoplimajr@gmail.com>
2022-07-17 23:45:15 -0300

    Refatorando o código do projeto para usar o Spring Data JPA

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -21,15 +21,12 @@ public class CadastroRestauranteService {
 	public Restaurante salvar(Restaurante restaurante) {
 		
 		Long cozinhaId = restaurante.getCozinha().getId();
-		
-		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		
-		if(cozinha == null) {
-			throw new EntidadeNaoEncontradaException(String.format("Cozinha não cadastrada com o código %d", cozinhaId));			
-		}
-		
+
+		Cozinha cozinha = cozinhaRepository.findById(cozinhaId).orElseThrow(() -> new EntidadeNaoEncontradaException(
+				String.format("Cozinha não cadastrada com o código %d", cozinhaId)));
+
 		restaurante.setCozinha(cozinha);
-		
+
 		return restauranteRepository.salvar(restaurante);
 	}
 
