e7ab9171849fadfd2a0f6e29c373c6f5d48b54f4
eraldoplimajr <eraldoplimajr@gmail.com>
2022-07-28 01:21:27 -0300

    Conhecendo os prefixos de query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -35,14 +35,34 @@ public class TesteController {
 		return cozinhaRepository.findByNome(nome);
 	}
 	
+	@GetMapping("cozinhas/exists")
+	public boolean cozinhaExists(String nome) {
+		return cozinhaRepository.existsByNome(nome);
+	}
+	
 	@GetMapping("restaurantes/por-taxa-frete")
 	public List<Restaurante> buscarRestaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
-		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+		return restauranteRepository.queryByTaxaFreteBetween(taxaInicial, taxaFinal);
 	}
 	
 	@GetMapping("restaurantes/por-nome")
 	public List<Restaurante> buscarRestaurantesPorNomeCozinha(String nome, Long cozinhaId) {
 		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
+	
+	@GetMapping("restaurantes/primeiro-por-nome")
+	public Optional<Restaurante> buscarPrimeiroRestaurantePorNome(String nome) {
+		return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
+	}
+	
+	@GetMapping("restaurantes/top2-por-nome")
+	public List<Restaurante> buscarTop2RestaurantesPorNome(String nome) {
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("restaurantes/count-por-cozinha")
+	public int restaurantesCountPorCozinha(Long cozinhaId) {
+		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
 	  
 }
