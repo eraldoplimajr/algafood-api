e8796173133244f0b39497aa4395ef396760704c
eraldoplimajr <eraldoplimajr@gmail.com>
2023-01-26 01:03:57 -0300

    9.2. Adicionando constraints e validando no controller com @Valid

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -14,11 +14,10 @@ import lombok.Getter;
 public class Problem {
 	
 	private Integer status;
+	private LocalDateTime timestamp;
 	private String type;
 	private String title;
 	private String detail;
-	
 	private String userMessage;
-	private LocalDateTime timestamp;
 	
 }
