539fe2732831ed485844c434a2a6741520557a3b
eraldoplimajr <eraldoplimajr@gmail.com>
2023-01-27 23:38:36 -0300

    9.3. Desafio- tratando exception de violação de constraints de validação

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,6 +30,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocoreu um erro interno inesperado no sitema. "
 			+ "Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.build();		
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+		
+	}
+	
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleGeneralException(Exception ex, WebRequest request){
--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -10,7 +10,8 @@ public enum ProblemType {
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"), 
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
-	ERRO_DE_SISTEMA("/erro-sistema", "Erro interno do sistema");
+	ERRO_DE_SISTEMA("/erro-sistema", "Erro interno do sistema"),
+	DADOS_INVALIDOS("dados-invalidos", "Dados inválidos");
 	
 	private String title;
 	private String uri;
