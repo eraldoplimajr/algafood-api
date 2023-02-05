792996fd97e2ee3163df43ab25ff2c721972731a
eraldoplimajr <eraldoplimajr@gmail.com>
2023-02-04 22:55:43 -0300

    9.17. Criando constraints de validação customizadas em nível de classe

---
+++ src/main/java/com/algaworks/algafood/core/validation/ValorZeroIncluiDescricao.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.core.validation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {ValorZeroIncluiDescricaoValidator.class})
+public @interface ValorZeroIncluiDescricao {
+	
+	String message() default "descrição obrigatória inválida";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+
+	String valorField();
+
+	String descricaoField();
+
+	String descricaoObrigatoria();
+
+}
