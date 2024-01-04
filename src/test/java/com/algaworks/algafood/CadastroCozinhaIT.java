3311a67d0345735bd8b10ed3a828e6846acf048f
eraldoplimajr <eraldoplimajr@gmail.com>
2024-01-03 23:07:13 -0300

    10.9 Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,12 +1,14 @@
 package com.algaworks.algafood;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
@@ -24,6 +26,21 @@ public class CadastroCozinhaIT {
                 .when()
                 .get()
                 .then().statusCode(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Tailandesa"));
 
     }
 
