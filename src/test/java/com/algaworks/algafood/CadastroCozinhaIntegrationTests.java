225e4c06da26183d24f8d4a1acd977a0008d477c
eraldoplimajr <eraldoplimajr@gmail.com>
2024-01-03 22:01:48 -0300

    10.5 Desafio - escrevendo testes de integração falha excluir cozinha

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
@@ -20,16 +22,14 @@ public class CadastroCozinhaIntegrationTests {
     @Test
     public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
 
-        // cenário
         Cozinha novaCozinha = new Cozinha();
         novaCozinha.setNome("Chinesa");
 
-        // ação
         novaCozinha = cadastroCozinha.salvar(novaCozinha);
 
-        // validação
         assertNotNull(novaCozinha);
         assertNotNull(novaCozinha.getId());
+
     }
 
     @Test
@@ -40,6 +40,28 @@ public class CadastroCozinhaIntegrationTests {
 
         Exception exception = assertThrows(ConstraintViolationException.class, () -> {
             cadastroCozinha.salvar(novaCozinha);
+        });
+
+    }
+
+    @Test
+    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+
+        Cozinha novaCozinha = new Cozinha();
+
+        Exception exception = assertThrows(CozinhaNaoEncontradaException.class, () -> {
+            cadastroCozinha.excluir(100L);
+        });
+
+    }
+
+    @Test
+    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+
+        Cozinha novaCozinha = new Cozinha();
+
+        Exception exception = assertThrows(EntidadeEmUsoException.class, () -> {
+            cadastroCozinha.excluir(1L);
         });
 
     }
