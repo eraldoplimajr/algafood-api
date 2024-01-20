8a9c267c121a5d1694789c2ff3d4da7a057f1934
eraldoplimajr <eraldoplimajr@gmail.com>
2024-01-19 23:13:53 -0300

    11.7 Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +28,9 @@ public class RestauranteMixin {
     @JsonIgnore
     private Endereco endereco;
 
-    @JsonIgnore
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
-    @JsonIgnore
-    private LocalDateTime dataAtualizacao;
+    private OffsetDateTime dataAtualizacao;
 
     @JsonIgnore
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
