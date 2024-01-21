4015f063f643ded6007111915bb49c3757171493
eraldoplimajr <eraldoplimajr@gmail.com>
2024-01-21 11:37:04 -0300

    11.14 Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/algafood/api/converter/RestauranteModelConverter.java
+++ src/main/java/com/algaworks/algafood/api/converter/RestauranteModelConverter.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.converter;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,17 +13,11 @@ import java.util.stream.Collectors;
 @Component
 public class RestauranteModelConverter {
 
-    public static RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
+    @Autowired
+    private ModelMapper modelMapper;
 
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(cozinhaModel);
-        return restauranteModel;
+    public RestauranteModel toModel(Restaurante restaurante) {
+        return modelMapper.map(restaurante, RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes){
