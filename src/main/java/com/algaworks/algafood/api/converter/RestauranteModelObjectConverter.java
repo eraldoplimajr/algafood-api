eef221967f4e5916b8137f577a1a6e6c53851b16
eraldoplimajr <eraldoplimajr@gmail.com>
2025-06-27 23:04:58 -0300

    Correção de erro de na alteração de Restaurante com id de Cozinha alterado

--- src/main/java/com/algaworks/algafood/api/converter/RestauranteModelObjectConverter.java
+++ src/main/java/com/algaworks/algafood/api/converter/RestauranteModelObjectConverter.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.converter;
 
 import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
@@ -22,6 +23,10 @@ public class RestauranteModelObjectConverter {
         // para evitar org.hibernate.HibernateException:
         // identifier of an instance of com.algaworks.algafood.domain.model.Cozinha was altered from 2 to 1
         restaurante.setCozinha(new Cozinha());
+
+        if(restaurante.getEndereco() != null){
+            restaurante.getEndereco().setCidade(new Cidade());
+        }
 
         modelMapper.map(restauranteInput, restaurante);
     }
