a443b1a5f38e5e949f23dfa5455a6f1d138cb650
eraldoplimajr <eraldoplimajr@gmail.com>
2026-02-16 20:52:57 -0300

    12.19. PRÉ Desafio - Inserido massa de dados para pedido e item pedido

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -6,16 +6,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -39,7 +30,7 @@ public class Pedido {
 	@Embedded
 	private Endereco enderecoEntrega;
 	
-	@Enumerated
+	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
 
 	@CreationTimestamp
