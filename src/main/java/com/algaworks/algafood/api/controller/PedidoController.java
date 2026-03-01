8c8f7e4127442d5f41eeb8a7bd4e808199be2c7c
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-01 20:45:08 -0300

    13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -8,8 +8,12 @@ import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.model.input.PedidoInput;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -33,9 +37,32 @@ public class PedidoController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+
+        List<Pedido> pedidos = cadastroPedido.listarTodos();
+        List<PedidoResumoModel> pedidosModel = pedidoResumoModelConverter.toCollectionModel(pedidos);
+
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if (StringUtils.isNotBlank(campos)) {
+            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
+    }
+
+/*
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<PedidoResumoModel> listar() {
         return pedidoResumoModelConverter.toCollectionModel(cadastroPedido.listarTodos());
     }
+*/
 
     @GetMapping("/{codigoPedido}")
     @ResponseStatus(HttpStatus.OK)
