d0dc2d2e00d0e12ff9bdcd288da8d176b9ae565c
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-07 01:07:25 -0300

    13.19. Estruturando endpoint e serviço de emissão de relatório em PDF

--- src/main/java/com/algaworks/algafood/infrastructure/repository/service/query/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/query/VendaQueryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.infrastructure.repository.service;
+package com.algaworks.algafood.infrastructure.repository.service.query;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/report/PdfVendaReportService.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.infrastructure.repository.service.report;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.service.VendaReportService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfVendaReportService implements VendaReportService {
+
+    @Override
+    public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffSet) {
+        return null;
+    }
+
+}
