20710adc4cacb09f0c6d57571a7f1f7ba9629459
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-08 14:07:32 -0300

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/infrastructure/repository/service/report/PdfVendaReportService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/report/PdfVendaReportService.java
@@ -1,15 +1,43 @@
 package com.algaworks.algafood.infrastructure.repository.service.report;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.service.VendaQueryService;
 import com.algaworks.algafood.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 @Service
 public class PdfVendaReportService implements VendaReportService {
 
+    @Autowired
+    private VendaQueryService vendaQueryService;
+
     @Override
     public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffSet) {
-        return null;
+
+        try {
+            var inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+
+            var parametros = new HashMap<String, Object>();
+            parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            var vendasDiarias = vendaQueryService.consultarVendasDiarias(filtro, timeOffSet);
+            var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (JRException e) {
+            throw new ReportException("Não foi possível emitir relatório de vendas diárias", e);
+        }
     }
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/report/ReportException.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.infrastructure.repository.service.report;
+
+public class ReportException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ReportException(String message) {
+        super(message);
+    }
+
+    public ReportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
