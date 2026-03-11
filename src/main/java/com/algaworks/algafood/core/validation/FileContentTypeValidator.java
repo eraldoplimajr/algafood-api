f6739c4d022584906fac77c47236975b9ae0c2c6
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-11 00:03:22 -0300

    14.4. Desafio- Ajustes

--- src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
+++ src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -11,29 +11,16 @@ import java.util.List;
 
 public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
 
-	private String[] allowed;
+	private List<String> allowedContentTypes;
 
 	@Override
 	public void initialize(FileContentType constraintAnnotation) {
-		this.allowed = constraintAnnotation.allowed();
+		this.allowedContentTypes = Arrays.asList(constraintAnnotation.allowed());
 	}
 
 	@Override
 	public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
-
-		return multipartFile.getContentType().contains(MediaType.IMAGE_JPEG_VALUE) || multipartFile.getContentType().contains(MediaType.IMAGE_PNG_VALUE);
-
+		return multipartFile == null || this.allowedContentTypes.contains(multipartFile.getContentType());
 	}
-
-
-	public boolean isValid(String[] strings, ConstraintValidatorContext constraintValidatorContext) {
-		List<String> list = Arrays.asList(strings);
-
-		boolean b = list.stream()
-				.allMatch(s -> s.contains(MediaType.IMAGE_JPEG_VALUE) || s.contains(MediaType.IMAGE_PNG_VALUE));
-
-		return b;
-	}
-
 
 }
