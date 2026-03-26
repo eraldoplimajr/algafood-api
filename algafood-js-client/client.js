5ae21784337622f84bf74ad851187d2cd232a96b
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-25 21:54:31 -0300

    16.5. Entendendo o funcionamento de preflight do CORS

--- algafood-js-client/client.js
+++ algafood-js-client/client.js
@@ -9,4 +9,15 @@ function consultarRestaurantes() {
   });
 }
 
+function fecharRestaurante() {
+  $.ajax({
+    url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
+    type: "put",
+
+    success: function(response) {
+      alert("Restaurante foi fechado!");
+    }
+  });
+}
+
 $("#botao").click(consultarRestaurantes);
