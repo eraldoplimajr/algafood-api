0bc232fe432a43a9a09d2054d84735bd6f41ab63
eraldoplimajr <eraldoplimajr@gmail.com>
2026-03-25 22:02:51 -0300

    16.6. Habilitando CORS globalmente no projeto da API

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -31,7 +31,6 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
